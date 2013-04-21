1de188c25cdf40408b49a0c577c50604793b6d87
gordianyuan <gordianyuan@gmail.com>
2013-04-22 00:23:22 +0800

    fixed black flash bug

--- colorback.js
+++ colorback.js
@@ -1,4 +1,9 @@
-$("html")
-	.add("body")
-	.add("body>div")
-	.css("-webkit-filter", "grayscale(0)");
+var elements = $("html").add("body").add("body>div:first");
+elements.each(function(){
+	var filter = $(this).css("-webkit-filter");
+	if(filter && filter != "none"){
+		if(filter.match(/^grayscale/)){
+			$(this).css("-webkit-filter", "grayscale(0)");
+		}
+	}
+});
